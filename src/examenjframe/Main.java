5a5a9791aebc51fa6ca0c0efcdeb24f48bf2d333
Pinelo <pinelo93@gmail.com>
2014-10-18 18:46:10 -0500

    first commit

---
+++ src/examenjframe/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package examenjframe;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Casa
+ */
+public class Main {
+    public static void main(String[] args) {
+        Juego jueJuego = new Juego();
+        jueJuego.setVisible(true);
+        jueJuego.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+    
+}
